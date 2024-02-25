86d8e9a4c2ddefff9b90bea530ac73fd2344a1fc
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-24 20:43:13 -0700

    Add profile, listings, and requested pages

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="bg-white dark:bg-black w-full p-24 rounded-xl text-brand">
+      profile Page
+    </div>
+  );
+}

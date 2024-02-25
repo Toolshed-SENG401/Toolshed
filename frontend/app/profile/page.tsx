f578e28029b02ec0ff9dd2493d345c9b623e3432
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-24 20:50:01 -0700

    Add profile page components

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import PersonalDetails from "./components/PersonalDetails";
+import ContactDetails from "./components/ContactDetails";
+import Rating from "./components/Rating";
+import Biography from "./components/Biography";
 
 export default function page() {
   return (
-    <div className="bg-white dark:bg-black w-full p-24 rounded-xl text-brand">
-      profile Page
+    // TODO: responsive design
+    <div className="bg-white dark:bg-black w-full p-24 rounded-xl text-brand flex flex-row justify-between">
+      <PersonalDetails></PersonalDetails>
+      <div className=" flex flex-col gap-8">
+        <ContactDetails></ContactDetails>
+        <Rating></Rating>
+      </div>
+      <Biography></Biography>
     </div>
   );
 }

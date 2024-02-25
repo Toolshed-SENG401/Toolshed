5e7bc293082c740013e60c14147d780997251d7a
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-24 21:55:13 -0700

    Add EditButton component to Biography

--- frontend/app/profile/components/Biography.tsx
+++ frontend/app/profile/components/Biography.tsx
@@ -1,19 +1,19 @@
 import { Label, Textarea } from "flowbite-react";
+import EditButton from "./EditButton";
 
 export default function Biography() {
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="text-xl font-medium">Biography</h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-xl font-medium">Biography</h1>
+        <EditButton></EditButton>
+      </div>
       <div className="flex flex-col gap-4 text-black dark:text-white">
         <div className="max-w-md">
-          <div className="mb-2 block">
-            <Label htmlFor="comment" value="Your message" />
-          </div>
           <Textarea
             id="comment"
             placeholder="Enter a description..."
-            required
-            rows={4}
+            rows={12}
             className=" bg-purple-200 text-brand shadow "
           />
         </div>
